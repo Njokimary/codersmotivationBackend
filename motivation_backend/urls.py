0f6177c4120951ceb7267220f3c0417b469f5132
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-09-15 19:09:36 +0300

    renamed the ptroject to motivation_backend

--- motivation_backend/urls.py
+++ motivation_backend/urls.py
@@ -1,4 +1,4 @@
-"""codersmotivation_backend URL Configuration
+"""motivation_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
