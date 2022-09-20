88527b6eabb884c3aa50e94179b983d81ae65eda
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-09-20 23:14:28 +0300

    created token generator token

--- authentication/models.py
+++ authentication/models.py
@@ -8,5 +8,30 @@ class User(AbstractUser):
   #Boolean fields to select the type of account.
   is_admin = models.BooleanField(default=False)
   is_staff= models.BooleanField(default=False)
-  def __str__(self):
-      return self.username
+
+
+  @property
+  def token(self):
+      """
+      Allows us to get a user's token by calling `user.token` instead of
+      `user.generate_jwt_token().
+
+      The `@property` decorator above makes this possible. `token` is called
+      a "dynamic property".
+      """
+      return self._generate_jwt_token()
+
+
+  def _generate_jwt_token(self):
+      """
+      Generates a JSON Web Token that stores this user's ID and has an expiry
+      date set to 60 days into the future.
+      """
+      dt = datetime.now() + timedelta(days=60)
+
+      token = jwt.encode({
+          'id': self.pk,
+          'exp': int(dt.strftime('%s'))
+      }, settings.SECRET_KEY, algorithm='HS256')
+
+      return token.decode('utf-8')
