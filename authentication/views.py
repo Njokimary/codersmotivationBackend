b70e569211a37ae53667a12163c47a7e9945f16c
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-09-26 13:31:19 +0300

    created user Registration View

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from serializer import UserRegistrationSerializer, LoginSerializer
+from rest_auth.registration.views import RegisterView
+
 
 # Create your views here.
 
 def index(self):
     return render(self,"index.html")
+
+class userRegistrationView(RegisterView):
+    serializer_class =UserRegistrationSerializer
