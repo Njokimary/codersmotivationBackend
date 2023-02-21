2a2a4e99565e063f883af330736f62d38c679e93
ELIJAH KIRAGU <eliki13720@gmail.com>
2023-02-21 23:11:59 +0300

    added  comments url

--- codersmotivation/urls.py
+++ codersmotivation/urls.py
@@ -6,10 +6,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     # path('home', views.home ,name="home")
     path('api/post/', views.Postapi.as_view()),
-    # path('api/profile/', views.profile_update.as_view()),
-    # path('update_profile/<int:pk>/', views.UpdateProfileView.as_view(), name='auth_update_profile'),
-    path('api/like/',views.Like .as_view()),
-    # path('likes/', views.LikeAPIView.as_view(), name='like_api'),
+    path('comments/', views.CommentAPIView.as_view(), name='comment_api'),
     path('posts/<int:pk>/like/', LikeView.as_view()),
     path('posts/<int:pk>/unlike/', UnlikeView.as_view()),
 ]
